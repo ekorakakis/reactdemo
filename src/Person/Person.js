6d4a28f0ec23985e8fefd5b32c291344dc50dee8
manos <ekorakakis@gmail.com>
2020-03-09 22:31:10 +0000

    #First attempt

---
+++ src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//function person(){
+//return <h2></h2>
+//}
+//or
+//var person = function() {
+//return ...
+//}
+
+// ES6 function syntax (arrow)
+const person = () => {
+    return <p>I'm a person!</p>
+}
+
+export default person;
